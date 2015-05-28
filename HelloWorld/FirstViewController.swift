4da1e94a9575f3f2d21d2136bee48f30cea49535
Ruchita Sarawgi <ruchita@vimeo.com>
2015-05-28 12:14:55 -0400

    Initial Commit

---
+++ HelloWorld/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HelloWorld
+//
+//  Created by Sarawgi, Ruchita on 5/28/15.
+//  Copyright (c) 2015 Vimeo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
